412513cf7dea665517cf5089fe4324ec3da32c8e
Markus Schober <markus.e.schober@gmail.com>
2013-07-30 12:39:04 -0400

    Clean up syntax slightly

--- emacs.d/packages.el
+++ emacs.d/packages.el
@@ -47,10 +47,8 @@
 (autoload 'camldebug "camldebug" "Run the Caml debugger" t)
 (autoload 'tuareg-imenu-set-imenu "tuareg-imenu" "Configuration of imenu for tuareg" t)
 (add-hook 'tuareg-mode-hook 'tuareg-imenu-set-imenu)
-(setq auto-mode-alist 
-	  (append '(("\\.ml[ily]?$" . tuareg-mode)
-				("\\.topml$" . tuareg-mode))
-			  auto-mode-alist))
+(add-to-list 'auto-mode-alist '("\\.ml[ily]?$" . tuareg-mode))
+(add-to-list 'auto-mode-alist '("\\.topml$" . tuareg-mode))
 
 ;; highlight-sexp
 (add-to-load-path "~/.emacs.d/highlight-sexp/")
