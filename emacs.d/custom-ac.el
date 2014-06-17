98b6c6ff70d2d5d0c773ef2bb80535ac94e84ab0
Markus Schober <markus.e.schober@gmail.com>
2014-06-16 23:42:53 -0400

    custom-ac works now, theoretically

--- emacs.d/custom-ac.el
+++ emacs.d/custom-ac.el
@@ -1,5 +1,12 @@
 (provide 'custom-ac)
 
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(ac-config-default)
+
+(custom-set-variables
+ '(ac-ispell-requires 4))
+
 (eval-after-load "auto-complete"
   '(progn
      (ac-ispell-setup)))
@@ -9,3 +16,5 @@
 
 (add-hook 'git-commit-mode-hook 'my/enable-ac-ispell)
 (add-hook 'mail-mode-hook 'my/enable-ac-ispell)
+(add-hook 'text-mode-hook 'my/enable-ac-ispell)
+
