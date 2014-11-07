00834a60c22b83603d2fee1b82898f3ea470f631
Markus Schober <markus.e.schober@gmail.com>
2014-11-07 02:26:35 -0500

    Emacs: re-enable custom-ac (which wasn't the culprit)

--- emacs.d/init/custom-ac.el
+++ emacs.d/init/custom-ac.el
@@ -1,20 +1,20 @@
 (provide 'custom-ac)
 
-;; (require 'auto-complete-config)
-;; (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
-;; (ac-config-default)
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(ac-config-default)
 
-;; (custom-set-variables
-;;  '(ac-ispell-requires 4))
+(custom-set-variables
+ '(ac-ispell-requires 4))
 
-;; (eval-after-load "auto-complete"
-;;   '(progn
-;;      (ac-ispell-setup)
-;;      (require 'go-autocomplete)))
+(eval-after-load "auto-complete"
+  '(progn
+     (ac-ispell-setup)
+     (require 'go-autocomplete)))
 
-;; (defun my/enable-ac-ispell ()
-;;   (add-to-list 'ac-sources 'ac-source-ispell))
+(defun my/enable-ac-ispell ()
+  (add-to-list 'ac-sources 'ac-source-ispell))
 
-;; (add-hook 'git-commit-mode-hook 'my/enable-ac-ispell)
-;; (add-hook 'mail-mode-hook 'my/enable-ac-ispell)
-;; (add-hook 'text-mode-hook 'my/enable-ac-ispell)
+(add-hook 'git-commit-mode-hook 'my/enable-ac-ispell)
+(add-hook 'mail-mode-hook 'my/enable-ac-ispell)
+(add-hook 'text-mode-hook 'my/enable-ac-ispell)
